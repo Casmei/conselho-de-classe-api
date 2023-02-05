cc7c0c2dcec761fae173f729ede06979a70af862
Casmei <casmei@protonmail.com>
2023-02-05 17:11:01 -0300

    fix(dockerFile): change env

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { ValidationPipe } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
@@ -18,10 +16,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(
-    configService.get('BACKEND_PORT')
-      ? Number(configService.get('BACKEND_PORT'))
-      : 3000,
-  );
+  await app.listen(3000);
 }
 bootstrap();
